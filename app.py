27c6c0324c4b00c07bc4b29f71bad31da2cda758
N07070 <nelson.perdriau@gmail.com>
2015-10-19 13:00:27 +0200

    Added all the code for the start of the REST Full API

---
+++ app.py
@@ -0,0 +1,40 @@
+#!flask/bin/python
+from flask import Flask
+from flask.ext.jsonpify import jsonify # Flask-Jsonpify 1.0
+
+app = Flask(__name__)
+
+# The "database"
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.route('/credits', methods=['GET'])
+def print_credits():
+    return "API v1.0 - Made by N07070"
+
+if __name__ == '__main__':
+    app.run(debug=True)
